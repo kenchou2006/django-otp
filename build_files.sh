5b88ee73b3af00914e5cfcdd2594b80dc954ffaa
周恭煥 <kenchou2006@gmail.com>
2024-09-27 16:26:19 +0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
