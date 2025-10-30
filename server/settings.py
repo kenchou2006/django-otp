e10b1a69c47f01f4a93e4a0315d9a3c46a82e156
周恭煥 <kenchou2006@gmail.com>
2025-10-30 19:40:16 +0800

    Clean up settings.py by removing comments
    Removed commented-out code from settings.py.

--- server/settings.py
+++ server/settings.py
@@ -51,7 +51,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'server.wsgi.application'
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -72,7 +72,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
